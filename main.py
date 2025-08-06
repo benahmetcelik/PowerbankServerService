6f7a131e4d08257b7e655ab58d2fcd35fff59106
ahmet.celik <ahmet.celik@tripy.mobi>
2025-08-06 20:27:14 +0300

    response status code changed to body

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def post_data(data):
     headers = {'Content-Type': 'application/json'}
     try:
         response = requests.post(url, data=json.dumps(data), headers=headers)
-        print(f"Status: {response.status_code}")
+        print(f"Status: {response.content}")
     except Exception as e:
         print(f"Error posting data: {e}")
 
